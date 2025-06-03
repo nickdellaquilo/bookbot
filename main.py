38336f6803d7481cf36b0ebce13c4b8689787044
Nick Dell'Aquilo <nickdellaquilo@proton.me>
2025-06-02 22:47:16 -0400

    add command line arguments, support for custom filepath

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import num_words, char_count, sorted_char_count
 
 def get_book_text(filepath):
@@ -6,7 +7,10 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) < 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
     text = get_book_text(filepath)
     print("============ BOOKBOT ============")
     print("Analyzing book found at " + filepath)
